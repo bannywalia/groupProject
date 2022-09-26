7c33bd5291a8b72fb2723f2d0439481158548ee2
bannywalia <110945553+bannywalia@users.noreply.github.com>
2022-09-26 18:37:08 -0400

    Revert "update to JAva to make it work"

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let search;
 let artistArray = [];
-const searchContainerEl = $('#search-results-container');
+const searchContainerEl = $('#search-results');
+const searchResults = $('#search-results');
 const artistEl = $("#artist");
 const searchFormEl = $('#search-form');
 const searchFieldEl = $("#search-field")
 
-
-var formSubmitHandler = function (event) {
+let formSubmitHandler = function (event) {
   event.preventDefault();
 
-  var artist = artistInputEl.value.trim();
+  let artist = searchFieldEl.val().trim();
 
   if (artist) {
     getArtistName(artist);
@@ -21,14 +21,22 @@ var formSubmitHandler = function (event) {
   }
 };
 
-var getArtist = function (searchTerm) {
-  var apiUrl = 'https://youtube-music1.p.rapidapi.com/v2/search?query=' + searchTerm;
+let getArtistName = function (art) {
+  let apiUrl = 'https://youtube-music1.p.rapidapi.com/v2/search?query=' + art;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '0cc9ca8730msh65c66718893a8f3p1fbbe6jsne7b5e98ffee6',
+      'X-RapidAPI-Host': 'youtube-music1.p.rapidapi.com'
+    }
+  };
 
-  fetch(apiUrl)
+  fetch(apiUrl, options)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          displayRepos(data, searchTerm);
+          console.log(data.result.songs)
+          displayArtists(data.result.songs, art);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -38,3 +46,41 @@ var getArtist = function (searchTerm) {
       alert('Unable to find Artist');
     });
 };
+
+let displayArtists = function (songs, searchTerm) {
+  if (songs.length === 0) {
+    searchContainerEl.textContent = 'No songs found.';
+    return;
+  }
+
+  searchResults.textContent = searchTerm;
+
+  // need to fix after this
+  for (let i = 0; i < songs.length; i++) {
+    let artistName = songs[i].name;
+
+    let artistNameEl = document.createElement('p')
+    artistNameEl.classList = 'list-item flex-row justify-space-between align-center';
+
+    let titleEl = document.createElement('span');
+    titleEl.innerHTML = artistName;
+
+    artistNameEl.append(titleEl);
+
+    let statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    if (songs[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + songs[i].open_issues_count + ' issue(s)';
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    //artistNameEl.append(statusEl);
+
+    searchContainerEl.append(artistNameEl);
+  }
+};
+
+searchFormEl.on('submit', formSubmitHandler);
