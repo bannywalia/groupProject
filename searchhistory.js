502f73332fd70f9ee00ac6f3d0445c75dbbac5c3
Bannywalia <Banny.walia07@gmail.com>
2022-09-27 11:10:23 -0400

    added search history

---
+++ searchhistory.js
@@ -0,0 +1,34 @@
+const searchEl = document.getElementById("search-button")
+const searchArtistEl = document.getElementById("search-field")
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+const clearEl = document.getElementById("clear-history");
+const historyEl = document.getElementById("history");
+
+searchEl.addEventListener("click", function (){
+  const searchTerm = searchArtistEl.value;
+  getArtistName(searchTerm);
+  searchHistory.push(searchTerm);
+  localStorage.setItem("search", JSON.stringify(searchHistory));
+  renderSearchHistory();
+})
+
+clearEl.addEventListener("click", function () {
+  localStorage.clear();
+  searchHistory = [];
+  renderSearchHistory();
+})
+
+function renderSearchHistory() {
+  historyEl.innerHTML = "";
+  for (let i = 0; i < searchHistory.length; i++) {
+      const historyItem = document.createElement("input");
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute("class", "form-control d-block bg-white");
+      historyItem.setAttribute("value", searchHistory[i]);
+      historyItem.addEventListener("click", function () {
+          getArtistName(historyItem.value);
+      })
+      historyEl.append(historyItem);
+  }
+}
